db470e03a93d9193b36cdc6c35c1dca97dbf59df
will <will@zipcodes-MBP-8.fios-router.home>
2020-11-22 21:11:17 -0500

    passed both tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,21 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer forwardButton = 0;
+        Integer backwardsButton = 0;
+        int i = startIndex.intValue();
+        int j = startIndex.intValue();
+        int firstSong = 0;
+        int lastSong = playList.length - 1;
+        while (playList[i] != selection && playList[j] != selection) {
+            forwardButton++;
+            backwardsButton++;
+            if (i == lastSong){
+                i = firstSong;
+            } if (j == firstSong) {
+                j = lastSong;
+            } i++;
+        } j--;
+        return forwardButton < backwardsButton ? forwardButton : backwardsButton;
     }
 }
